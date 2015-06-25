ef03852589d920fa22ddf5c9cbe1115b1c5a547c
manuelhartl <manuel.hartl@gmail.com>
2015-06-25 22:58:03 +0200

    added link to login page when activating
    Signed-off-by: manuelhartl <manuel.hartl@gmail.com>

--- v1/activate.php
+++ v1/activate.php
@@ -5,7 +5,7 @@ if (array_key_exists ( 'token', $_GET )) {
 	$token = $_GET ['token'];
 	$pdo = db_open ();
 	if (activate ( $pdo, $token )) {
-		echo "activated, now login";
+		echo 'activated, now login - <a href="index.php">Login</a>';
 		die ();
 	}
 }
