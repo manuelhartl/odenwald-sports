b4a4923544ddd4811b93c7e6e31129a39e790ca9
manuelhartl <manuel.hartl@gmail.com>
2015-06-27 17:58:19 +0200

    - tests with dropdown menu
    Signed-off-by: manuelhartl <manuel.hartl@gmail.com>

--- v1/pages/navigation.php
+++ v1/pages/navigation.php
@@ -1,11 +1,25 @@
 <div id="navigation">
 	<form name="logout" action="index.php" method="post">
-		<input name="action" type="hidden" value="home" /> <input
-			id="submit" name="submit" type="submit" value="Start" />
+		<input name="action" type="hidden" value="home" /> <input id="submit"
+			name="submit" type="submit" value="Start" />
 	</form>
-	
-	<form name="logout" action="index.php" method="post">
-		<input name="action" type="hidden" value="logout" /> <input
-			id="submit" name="submit" type="submit" value="Logout" />
-	</form>	
+
+	<!-- 
+	<button class="btn btn-default dropdown-toggle" type="button"
+		id="dropdownMenuAccount" data-toggle="dropdown" aria-haspopup="true"
+		aria-expanded="true">
+		Account <span class="caret"></span>
+	</button>
+	<ul class="dropdown-menu dropdown-menu-right"
+		aria-labelledby="dropdownMenuAccount">
+		<li>
+		 -->
+			<form name="logout" action="index.php" method="post">
+				<input name="action" type="hidden" value="logout" /> <input
+					id="submit-logout" name="submit" type="submit" value="Logout" />
+			</form>
+			<!-- 
+		</li>
+	</ul>
+	 -->
 </div>
