da20d88c4d90ac7ba0e9adfdef9098c811af361f
manuelhartl <manuel.hartl@gmail.com>
2015-06-28 22:54:07 +0200

    - automatic redirection when activating
    Signed-off-by: manuelhartl <manuel.hartl@gmail.com>

--- v1/activate.php
+++ v1/activate.php
@@ -1,13 +1,33 @@
 <?php
+function htmlHeader($redirect) {
+	?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="refresh"
+	content="<?php echo $redirect; ?>; url=index.php">
+<title>Touren</title>
+</head>
+<body>
+<?php
+}
+
 require_once 'lib/db_users.php';
 
 if (array_key_exists ( 'token', $_GET )) {
 	$token = $_GET ['token'];
 	$pdo = db_open ();
+	$redirectInSeconds;
 	if (activate ( $pdo, $token )) {
-		echo 'activated, now login - <a href="index.php">Login</a>';
-		die ();
+		$redirectInSeconds = 2;
+		echo 'activated ';
+	} else {
+		$redirectInSeconds = 180;
+		echo 'activation failed - contact admin';
 	}
+	htmlHeader($redirectInSeconds);
+	echo '- you will be redirected to the login page in '.$redirectInSeconds.' seconds (or go manually there: <a href="index.php">Login</a>)';
 }
-echo "activation failed";
 ?>
+</body>
+</html>
