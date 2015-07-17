76868455f1d27c0a0e924ba7634df1d323ed70f6
Gerald Wischnewski <Gerald.Wischnewski@gmx.de>
2015-07-17 03:46:51 +0200

    add config parameter beta

--- v1/pages/footer.php
+++ v1/pages/footer.php
@@ -1,3 +1,11 @@
 <?php
-echo '<div id="version"><a href="html/disclaimer.php" target="_blank">Regeln</a> <a href="html/impressum.php" target="_blank">Impressum</a> Version: ' . $config ['version'] . '</div>';
+echo '<div id="version">
+		<a href="html/disclaimer.php" target="_blank">Regeln</a>
+		<a href="html/impressum.php" target="_blank">Impressum</a>
+		Version: ';
+if($config ['beta']){
+	echo 'Beta ';
+}
+echo  $config ['version'];
+echo   '</div>';
 ?>
