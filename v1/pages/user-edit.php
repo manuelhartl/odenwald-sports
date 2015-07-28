a8c39df92a15642d97be3a1bf015b0a1ebd75954
Gerald Wischnewski <Gerald.Wischnewski@gmx.de>
2015-07-28 15:49:05 +0200

    html errors corrected

--- v1/pages/user-edit.php
+++ v1/pages/user-edit.php
@@ -54,6 +54,7 @@
         </script> -->
 				</div> (Dies ist f&uuml;r alle angemeldeten Benutzer sichtbar - Nur eintragen wenn dies in Ordnung ist)</td>
 		</tr>
+		<tr>
 		<td>Wohnanschrift)</td>
 		<td><input type="text" id="address" style="width: 100%" name="address"
 			value="<?php echo isset($input['address']) ? $input['address'] : '';?>" /> <input type="hidden" id="address-radius" />
@@ -93,3 +94,4 @@
 			</td>
 		</tr>
 	</table>
+</form>
