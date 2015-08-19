f5c579026253d4533271c695cca614e25588e440
GW-BIKE <gw_bike@gmx.de>
2015-08-19 10:44:04 +0200

    Typo

--- v1/pages/user-edit.php
+++ v1/pages/user-edit.php
@@ -19,26 +19,33 @@
 		<tr>
 			<td>Echter Name</td>
 			<td><input type="text" style="width: 100%" name="realname"
-				value="<?php echo isset($input['realname']) ? $input['realname'] : '';?>" /><br> (Dies ist f&uuml;r alle
-				angemeldeten Benutzer sichtbar - Nur eintragen wenn dies in Ordnung ist)</td>
+				value="<?php echo isset($input['realname']) ? $input['realname'] : '';?>" /><br>
+				(Dies ist f&uuml;r alle angemeldeten Benutzer sichtbar - Nur
+				eintragen wenn dies in Ordnung ist)</td>
 		</tr>
 		<tr>
 			<td>Telefon</td>
 			<td><input type="text" style="width: 100%" name="phone"
-				value="<?php echo isset($input['phone']) ? $input['phone'] : '';?>" /><br> (Dies ist f&uuml;r alle angemeldeten
-				Benutzer sichtbar - Nur eintragen wenn dies in Ordnung ist)</td>
+				value="<?php echo isset($input['phone']) ? $input['phone'] : '';?>" /><br>
+				(Dies ist f&uuml;r alle angemeldeten Benutzer sichtbar - Nur
+				eintragen wenn dies in Ordnung ist)</td>
 		</tr>
 		<tr>
 			<td>Geburtsjahr</td>
-			<td><input type="text" class="form-control" style="width: 150px;" id="birthdate" maxlength="10" name="birthdate"
-				value="<?php echo (Utilities::isValidDate($input ['birthdate'],"2000-1-1")->format ( 'Y' ));?>" /> (oder leerlassen)<br>(Dies
-				ist f&uuml;r alle angemeldeten Benutzer sichtbar - Nur eintragen wenn dies in Ordnung ist)</td>
+			<td><input type="text" class="form-control" style="width: 150px;"
+				id="birthdate" maxlength="10" name="birthdate"
+				value="<?php echo (Utilities::isValidDate($input ['birthdate'],"2000-1-1")->format ( 'Y' ));?>" />
+				(oder leerlassen)<br>(Dies ist f&uuml;r alle angemeldeten Benutzer
+				sichtbar - Nur eintragen wenn dies in Ordnung ist)</td>
 		</tr>
 		<tr>
-			<td>Wohnanschrift)</td>
-			<td><input type="text" id="address" style="width: 100%" name="address"
-				value="<?php echo isset($input['address']) ? $input['address'] : '';?>" /> <input type="hidden" id="address-radius" />
-				<input type="hidden" id="address-lat" name="address-lat" /><input type="hidden" id="address-lon" name="address-lon" />
+			<td>Wohnanschrift</td>
+			<td><input type="text" id="address" style="width: 100%"
+				name="address"
+				value="<?php echo isset($input['address']) ? $input['address'] : '';?>" />
+				<input type="hidden" id="address-radius" /> <input type="hidden"
+				id="address-lat" name="address-lat" /><input type="hidden"
+				id="address-lon" name="address-lon" />
 				<div id="address-map" style="width: 100%; height: 400px;"></div> <script>$('#address-map').locationpicker({
 	location: {latitude: <?php echo isset($input['address-lat']) ? $input['address-lat'] : '49.85212170040001';?>, longitude: <?php echo isset($input['address-lon']) ? $input['address-lon'] : '8.670546531677246';?>},	
 	radius: 50,
@@ -50,26 +57,30 @@
     },
 	enableAutocomplete: true
 	});
-</script><br> (Dies ist f&uuml;r alle angemeldeten Benutzer sichtbar - Nur eintragen wenn dies in Ordnung ist)</td>
+</script><br> (Dies ist f&uuml;r alle angemeldeten Benutzer sichtbar -
+				Nur eintragen wenn dies in Ordnung ist)</td>
 		</tr>
 		<tr>
 			<td>Tour Mailings</td>
-			<td><input name="mailing" type="checkbox" value="true" <?php echo $input['mailing'] ? 'checked' : '';?>></td>
+			<td><input name="mailing" type="checkbox" value="true"
+				<?php echo $input['mailing'] ? 'checked' : '';?>></td>
 		</tr>
 		<tr>
 			<td></td>
 			<td><input type="submit" value="Speichern" />
 				</form>
 				<form name="login" action="index.php" method="post">
-					<input name="action" type="hidden" value="home" /> <input name="submit" type="submit" value="Abbrechen" />
+					<input name="action" type="hidden" value="home" /> <input
+						name="submit" type="submit" value="Abbrechen" />
 				</form></td>
 		</tr>
 		<tr>
 			<td></td>
 			<td>
 				<form name="password-change" action="index.php" method="post">
-					<input name="action" type="hidden" value="password-change" /> <input id="submit-password-change" name="submit"
-						type="submit" value="Passwort &auml;ndern" />
+					<input name="action" type="hidden" value="password-change" /> <input
+						id="submit-password-change" name="submit" type="submit"
+						value="Passwort &auml;ndern" />
 				</form>
 			</td>
 		</tr>
