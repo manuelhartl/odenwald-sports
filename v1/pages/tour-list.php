5590453f2b159d0a8e8314e04001b410ab685d7c
manuelhartl <manuel.hartl@gmail.com>
2015-08-20 19:58:38 +0200

    removed commata from attendee list

--- v1/pages/tour-list.php
+++ v1/pages/tour-list.php
@@ -114,7 +114,6 @@ while ( $row = $stmt->fetch ( PDO::FETCH_ASSOC ) ) {
 	$authuserid = hasAuth () ? authUser ()->id : '';
 	$users = getAttendees ( $pdo, $tour->id );
 	$countAttendees = count ( $users );
-	$firstUser = true;
 	foreach ( $users as $user ) {
 		// $tourmember = DB_member::getMemberById ( $pdo, $user ['id'] );
 		$u = new DBUser ();
@@ -124,11 +123,10 @@ while ( $row = $stmt->fetch ( PDO::FETCH_ASSOC ) ) {
 		$ue = getDBUserExtraById ( $pdo, $u->id );
 		
 		$emailString = '<a id="tour-mail1" class="icon-attendee-mail" title="Mail an ' . $u->username . '" href="?action=mail-user&touserid=' . $u->id . '&subject=' . urlencode ( $tourDescription ) . '"> </a>';
-		$attendeeString = $attendeeString . '<div class="attendee"><div class="attendee_mail">' . $emailString . '</div><div class="attendee_profil">' . createUserProfilLink ( $u, "style='border: none; line-height: 18px;'", createUserInfo ( $u, $ue ) ) . (! $firstUser ? ', ' : '') . '</div></div> ';
+		$attendeeString = $attendeeString . '<div class="attendee"><div class="attendee_mail">' . $emailString . '</div><div class="attendee_profil">' . createUserProfilLink ( $u, "style='border: none; line-height: 18px;'", createUserInfo ( $u, $ue ) ) . '</div></div> ';
 		if ($user ['id'] == $authuserid) {
 			$joinedTour = true;
 		}
-		$firstUser = false;
 	}
 	
 	$startdate = $tour->startDateTime;
