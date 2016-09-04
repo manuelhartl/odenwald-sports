5e326a3f25beb1e46b8ed9793f84fc1eb176124b
Manuel Hartl <manuel.hartl@gmail.com>
2016-09-04 22:54:35 +0200

    tours.php update if not logged in

--- v1/rest/tours.php
+++ v1/rest/tours.php
@@ -2,10 +2,12 @@
 require_once __DIR__ . '/../lib/global.php';
 require_once __DIR__ . '/../lib/tours.php';
 
+/*
 if (!hasAuth()) {
 	echo "not logged in";
 	exit;
 }
+*/
 
 if ($_SERVER['REQUEST_METHOD'] != "GET"){
 	exit;
@@ -26,12 +28,13 @@ if (hasAuth ()) {
 }
 
 $stmt = $pdo->prepare ( 'select *,111195 * ST_Distance(POINT(?,?), meetingpoint_coord) as refm, t.status as tourstatus,t.id as id, g.id as guide, g.username as guidename' . //
-',X(meetingpoint_coord) as meetingpoint_lat,Y(meetingpoint_coord) as meetingpoint_long' . // 
+',X(meetingpoint_coord) as meetingpoint_lat,Y(meetingpoint_coord) as meetingpoint_long' . //
 ' from tour t' . //
 ' left join user g ON (t.fk_guide_id=g.id) ' . //
 ' left join sport_subtype ss ON (t.fk_sport_subtype_id=ss.id) ' . //
 ' left join sport s ON (ss.fk_sport_id=s.id) ' . //
 ' WHERE true ' . //
+(! hasAuth () ? ' AND t.status = "active"' : '') . //
 (' ORDER BY startdate ASC') . //
 '' ); //
 ex2er ( $stmt, array (
@@ -43,8 +46,10 @@ $tour_list = array();
 while ( $row = $stmt->fetch ( PDO::FETCH_ASSOC ) ) {
 	unset($tour);
 	// build tour
-
-	$tour['id']=$row ['id'];
+	if (hasAuth()) {
+		$tour['id']=$row ['id'];
+		$tour['canceled']  = $row ['tourstatus'] == 'canceled';
+	}
 	$tour['desc']=$row ['description'];
 	$tour['datetime'] = date_create ( $row ['startdate'] )->format(DateTime::ISO8601);
 	$tour['distance'] = $row['distance'];
@@ -52,27 +57,28 @@ while ( $row = $stmt->fetch ( PDO::FETCH_ASSOC ) ) {
 	$tour['duration'] = $row ['duration'];
 	$tour['elevation'] = $row ['elevation'];
 	$tour['skill'] = $row ['skill'];
-	$tour['canceled']  = $row ['tourstatus'] == 'canceled';
 	$tour['sport']['name'] = $row ['sportname'];
 	$tour['sport']['subname'] = $row ['sportsubname'];
-	$tour['meetingpoint']['name'] = $row ['meetingpoint'];
-	$tour['meetingpoint']['desc'] = $row ['meetingpoint_desc'];
-	if (isset ( $row ['meetingpoint_lat'] )) {
-		$tour['meetingpoint']['lat'] =  $row ['meetingpoint_lat'];
-	}
-	if (isset ( $row ['meetingpoint_long'] )) {
-		$tour['meetingpoint']['long'] = $row ['meetingpoint_long'];
-	}
-	$tour['guide']['id']=$row ['guide'];
-	$tour['guide']['name']= $row ['guidename'];
-
 	$users = getAttendees ( $pdo, $row ['id'] );
-	$tour['attendees']=array();
-	foreach ($users as $user) {
-		$attendee = array();
-		$attendee['id']=$user['id'];
-		$attendee['name']=$user['username'];
-		$tour['attendees'][]=$attendee;
+	$tour['attendees_count']=count($users);
+	if (hasAuth()) {
+		$tour['meetingpoint']['name'] = $row ['meetingpoint'];
+		$tour['meetingpoint']['desc'] = $row ['meetingpoint_desc'];
+		if (isset ( $row ['meetingpoint_lat'] )) {
+			$tour['meetingpoint']['lat'] =  $row ['meetingpoint_lat'];
+		}
+		if (isset ( $row ['meetingpoint_long'] )) {
+			$tour['meetingpoint']['long'] = $row ['meetingpoint_long'];
+		}
+		$tour['guide']['id']=$row ['guide'];
+		$tour['guide']['name']= $row ['guidename'];
+		$tour['attendees']=array();
+		foreach ($users as $user) {
+			$attendee = array();
+			$attendee['id']=$user['id'];
+			$attendee['name']=$user['username'];
+			$tour['attendees'][]=$attendee;
+		}
 	}
 	// push to array
 	$tour_list[]=$tour;
