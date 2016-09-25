93924c3663e29c2fee89b1ff65956339a5127ec3
Manuel Hartl <manuel.hartl@gmail.com>
2016-09-25 22:44:00 +0200

    add join, leave and cancel

---
+++ v1/rest/tour-leave.php
@@ -0,0 +1,35 @@
+<?php
+require_once __DIR__ . '/restlib.php';
+jsonCheckAuth ();
+
+if ($_SERVER ['REQUEST_METHOD'] != 'POST') {
+	exit ();
+}
+
+header ( 'Content-type: application/json' );
+$pdo = db_open ();
+
+$body = json_decode ( file_get_contents ( 'php://input' ), true );
+$tourid = $body ['id'];
+
+$json = array ();
+
+$tour = getDBTourById ( $pdo, $tourid );
+if (! $tour) {
+	$json ['error'] ['text'] = 'tourid is unknown';
+	http_response_code ( 400 );
+} else {
+	if ((! $tour->canceled) && ($tour->startDateTime >= new DateTime ())) {
+		tourLeave ( $pdo, authUser ()->id, $tourid );
+		http_response_code ( 200 );
+	} else {
+		http_response_code ( 500 );
+	}
+}
+
+$stmt = getTourStmt ( $pdo, $tourid );
+$row = $stmt->fetch ( PDO::FETCH_ASSOC );
+$json ['tour'] = row2tour ( $pdo, $row );
+echo json_encode ( $json );
+
+?>
